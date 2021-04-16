d97b4bf83ca5cffcb73ec523a64a04dd46dc212a
cherniaev egor <chrnyaevek@gmail.com>
2021-04-16 15:13:19 +0200

    fix urls

--- showmax/film_base/urls.py
+++ showmax/film_base/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/film/<str:id>', views.film, name='film'),
-    path('/group/<str:id>', views.group, name='group'),
+    path('film/<str:id>', views.film, name='film'),
+    path('group/<str:id>', views.group, name='group'),
 ]
