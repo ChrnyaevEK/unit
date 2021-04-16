882862de509c0c8ce66634bae424b0ca042b7ddc
cherniaev egor <chrnyaevek@gmail.com>
2021-04-16 13:30:38 +0200

    Index upd

--- showmax/film_base/views.py
+++ showmax/film_base/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 
 
+class Page:
+    web_public_name = 'Showmax'
+
+    def __init__(self, context):
+        self.context = context
+
+
 def index(request):
     template = loader.get_template('film_base/index.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render({'page': Page({
+        'title': 'Home',
+    })}, request))
