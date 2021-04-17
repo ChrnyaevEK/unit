505ae2e4aebffde617caf90560a29658f0645118
cherniaev egor <chrnyaevek@gmail.com>
2021-04-17 14:35:19 +0200

    upd

--- showmax/showmax/settings.py
+++ showmax/showmax/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-j#s)g)3grce^!^*!9f!^aww#*(d+zqr4-0ld*adr$n7158#awg'
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
